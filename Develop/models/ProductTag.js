5b690249a2afc32a9bd08bb0fb7f58d4d72a497d
Adina Henderson <adinahenderson.mail@gmail.com>
2021-06-10 17:44:55 -0400

    Defined columns for the ProductTag Module

--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,7 +6,27 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    // Define columns -- id, product id, referenced to Product model's id, tag id referenced to Tag model's id
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Product',
+        key: 'id',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'Tag',
+        key: 'id',
+      }
+    },
   },
   {
     sequelize,
